0e79654d312a12925fa5b5fe643f8291f89be0b9
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2025-12-19 20:56:42 -0800

    More tweaking

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -35,7 +35,9 @@ public class Elevator extends SubsystemBase {
         io.updateState(replayedInputs);
         // Must be called every periodic after updating hardware state.
         Logger.processInputs("Elevator", replayedInputs);
-        Logger.recordOutput("Elevator", currentElevatorSetpoint.getPosition().in(Inches));
+        Logger.recordOutput(
+                "Elevator/Desired Carriage Position",
+                currentElevatorSetpoint.getPosition().in(Inches));
     }
 
     @Override
@@ -55,7 +57,7 @@ public class Elevator extends SubsystemBase {
     public enum ElevatorHeight {
         // Positions taken from offseason bot code, inturn taken from onshape.
         UP(Inches.of(56.0)),
-        MIDDLE(Inches.of(5.0)),
+        MIDDLE(Inches.of(28.0)),
         DOWN(Inches.of(0.0));
 
         public final Distance position;
@@ -70,7 +72,8 @@ public class Elevator extends SubsystemBase {
 
         public Angle getPositionAngle() {
             // NOTE: yes this code is bs, but i wanna see it work first.
-            return Radians.of(getPosition().in(Inches) / (ElevatorConstants.ELEVATOR_HEIGHT_CHANGE_PER_MOTOR_ROTATION));
+            return Rotations.of(
+                    getPosition().in(Inches) / (ElevatorConstants.ELEVATOR_HEIGHT_CHANGE_PER_MOTOR_ROTATION));
         }
     }
 }
