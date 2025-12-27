e18c5544e0d04f44b74a618690a4ee47fa5197f8
codingman106 <jazl.ihsan@gmail.com>
2025-12-26 16:23:25 -0800

    implemented io

--- src/main/java/frc/robot/subsystems/groundintake/GroundIntakePivotIO.java
+++ src/main/java/frc/robot/subsystems/groundintake/GroundIntakePivotIO.java
@@ -1,4 +1,33 @@
 package frc.robot.subsystems.groundintake;
 
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface GroundIntakePivotIO {
+
+    @AutoLog
+    class GroundIntakePivotIOInputs {
+        public double pivotMotorRotations;
+        public double pivotMotorVelocityRotsPerSecond;
+        public double pivotMotorCurrent;
+        public double pivotMotorVoltage;
+    }
+
+    default void updateState(GroundIntakePivotIOInputs inputs) {}
+
+    default void setPivotMotorSetpoint(Angle setpoint) {}
+
+    default void setPivotMotorVoltage(double voltage) {}
+
+    default Angle getPivotMotorPosition() {
+        return Units.Rotations.of(0);
+    }
+
+    default Angle getGroundIntakePivotPosition() {
+        return Units.Degrees.of(0);
+    }
+
+
 }
